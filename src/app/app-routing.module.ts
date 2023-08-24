6bfc59f3651b45dad31efe5b7c0de0c5f7016184
Peter van der Wal <peterzakelijk@live.nl>
2023-08-24 22:18:42 +0200

    init example repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BakeryComponent} from "./bakery/bakery.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'bakery',
+  },
+  {path: 'bakery', component: BakeryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/bakery/bakery.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-bakery',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './bakery.component.html',
+  styleUrls: ['./bakery.component.scss']
+})
+export class BakeryComponent {
+  
+}
